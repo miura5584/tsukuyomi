e1fef6a4bdc19ce61da5bcb3eb0ba7f9603a5d06
northfieldzz <tommomo22>
2022-03-31 05:28:20 +0900

    add: database

--- main.go
+++ main.go
@@ -2,8 +2,24 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
+	"log"
 	"tsukuyomi/controller"
+	"tsukuyomi/ent"
 )
+
+func init() {
+	client, err := ent.Open("postgres", "host=localhost port=5432 user=postgres dbname=postgres password=postgres")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func(client *ent.Client) {
+		err := client.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(client)
+}
 
 func main() {
 	router := gin.Default()
